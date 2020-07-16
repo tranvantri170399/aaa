488aaefa2456526c603ba6ba034f3be77230f6ce
tritran <tranvantri.170399@gmail.com>
2020-07-16 15:52:14 +0700

    aaa

---
+++ Test Suites/Test Suite.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>104d217f-4769-437f-9e69-8bd36c376ed3</testSuiteGuid>
+   <testCaseLink>
+      <guid>e0f88523-1b2e-41fb-8d96-8717f09e7ff0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test1</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>8d9d4351-f0f2-47dd-91cd-1b01b52c8a21</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/data2</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>8d9d4351-f0f2-47dd-91cd-1b01b52c8a21</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>query</value>
+         <variableId>e387ff66-b226-495b-9d73-1e23985c6c0b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>8d9d4351-f0f2-47dd-91cd-1b01b52c8a21</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>feelinglucky</value>
+         <variableId>91b76401-170f-46e3-b82f-d9e9fb319af6</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
